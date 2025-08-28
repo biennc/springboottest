c881a3098014fcee7304c9da836aa3fce322c656
biennc <biennguyen3428@gmail.com>
2025-08-28 14:53:30 +0700

    remove unuse import

--- springboottest/src/main/java/com/example/springboottest/dtos/EmployeeUpdateDTO.java
+++ springboottest/src/main/java/com/example/springboottest/dtos/EmployeeUpdateDTO.java
@@ -3,7 +3,6 @@ import java.time.LocalDate;
 
 import com.example.springboottest.entities.Gender;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Pattern;
